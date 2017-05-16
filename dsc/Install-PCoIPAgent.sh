afdebf9e78d5ac0953178248ab30530eb0c0ff23
Frank Wang <fwang@teradici.com>
2017-05-16 15:17:14 -0700

    using unix EOL format

--- dsc/Install-PCoIPAgent.sh
+++ dsc/Install-PCoIPAgent.sh
@@ -3,15 +3,24 @@
 # the first argument is the Registration Code of PCoIP agent
 
 # Install the Teradici package key
-echo "Install the Teradici package key"
-sudo rpm --import https://downloads.teradici.com/rhel/teradici.pub.gpg# Add the Teradici repositoryecho "Add the Teradici repository"
-sudo wget -O /etc/yum.repos.d/pcoip.repo https://downloads.teradici.com/rhel/pcoip.repo
+echo "-->Install the Teradici package key"
+sudo rpm --import https://downloads.teradici.com/rhel/teradici.pub.gpg
+
+# Add the Teradici repository
+echo "-->Add the Teradici repository"
+sudo wget -O /etc/yum.repos.d/pcoip.repo https://downloads.teradici.com/rhel/pcoip.repo
+
 # Install the EPEL repository
-echo "Install the EPEL repository"
-sudo wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpmsudo rpm -i -q epel-release-latest-7.noarch.rpmif [ $? -ne 0 ]; then    # handle failurefi
+echo "-->Install the EPEL repository"
+sudo wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo rpm -i -q epel-release-latest-7.noarch.rpm
+
 # Install the PCoIP Agent
-echo "Install the PCoIP Agent"
+echo "-->Install the PCoIP Agent"
 sudo yum -y update
-sudo yum -y install pcoip-agent-standard# register license codeecho "register license code"
+sudo yum -y install pcoip-agent-standard
+
+# register license code
+echo "-->Register license code"
 pcoip-register-host --registration-code=@1
 pcoip-validate-license
