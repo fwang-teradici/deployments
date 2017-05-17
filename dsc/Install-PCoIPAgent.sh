1225fb37553f2d4ae6c7aa8eddd0ba73c0f88e88
Frank Wang <fwang@teradici.com>
2017-05-17 09:58:52 -0700

    try install desktop instead of whole gui

--- dsc/Install-PCoIPAgent.sh
+++ dsc/Install-PCoIPAgent.sh
@@ -4,7 +4,10 @@
 
 # Install desktop
 echo "-->Install desktop"
-sudo yum -y groupinstall "Server with GUI"
+# install the whole GUI and Additional Tools
+# sudo yum -y groupinstall "Server with GUI"
+# just install the gnome desktop 
+sudo yum groupinstall 'X Window System' 'GNOME'
 
 # Install the Teradici package key
 echo "-->Install the Teradici package key"
@@ -22,9 +25,48 @@ sudo yum -y install epel-release-latest-7.noarch.rpm
 # Install the PCoIP Agent
 echo "-->Install the PCoIP Agent"
 sudo yum -y update
-sudo yum -y install pcoip-agent-standard
+
+for idx in {1..3}
+do
+    sudo yum -y install pcoip-agent-standard
+    exitCode=$?
+    if [$exitCode -eq 0]
+    then
+        break
+    else
+        if [$idx -eq 3]
+        then
+            echo "failed to install pcoip agent."
+            exit $exitCode
+        else
+            sudo yum -y remove pcoip-agent-standard
+        fi
+    fi
+done
+    
 
 # register license code
 echo "-->Register license code"
-pcoip-register-host --registration-code=$1
-pcoip-validate-license
+for idx in {1..3}
+do
+    pcoip-register-host --registration-code=$1
+    pcoip-validate-license
+    if [$exitCode -eq 0]
+    then
+        break
+    else
+        if [$idx -eq 3]
+        then
+            echo "failed to register pcoip agent license."
+            exit $exitCode
+        fi
+    fi
+done
+
+# skip the gnome initial setup
+mkdir ~/.config
+echo "yes" >> ~/.config/gnome-initial-setup-done
+
+# start GUI
+sudo systemctl set-default graphical.target
+sudo systemctl start graphical.target
