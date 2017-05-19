0043d5090c2a9d2ca2fbb5a07846ca8b92a369a2
Frank Wang <fwang@teradici.com>
2017-05-19 10:36:14 -0700

    try to remove sudo

--- dsc/Install-PCoIPAgent.sh
+++ dsc/Install-PCoIPAgent.sh
@@ -4,24 +4,26 @@
 
 # Install the Teradici package key
 echo "-->Install the Teradici package key"
-sudo rpm --import https://downloads.teradici.com/rhel/teradici.pub.gpg
+rpm --import https://downloads.teradici.com/rhel/teradici.pub.gpg
 
 # Add the Teradici repository
 echo "-->Add the Teradici repository"
-sudo wget -O /etc/yum.repos.d/pcoip.repo https://downloads.teradici.com/rhel/pcoip.repo
+wget -O /etc/yum.repos.d/pcoip.repo https://downloads.teradici.com/rhel/pcoip.repo
 
-# Install the EPEL repository
-echo "-->Install the EPEL repository"
-sudo wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-sudo yum -y install epel-release-latest-7.noarch.rpm
+# Update the EPEL repository
+echo "-->Update the EPEL repository"
+rpm -Uvh --quiet https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+#wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+#yum -y install epel-release-latest-7.noarch.rpm
 
 # Install the PCoIP Agent
 echo "-->Install the PCoIP Agent"
-sudo yum -y update
+yum -y update
 
 for idx in {1..3}
 do
-    sudo yum -y install pcoip-agent-standard
+    yum -y install pcoip-agent-standard
     exitCode=$?
     
     if [ $exitCode -eq 0 ]
@@ -33,7 +35,7 @@ do
             echo "failed to install pcoip agent."
             exit $exitCode
         else
-            sudo yum -y remove pcoip-agent-standard
+            yum -y remove pcoip-agent-standard
         fi
     fi
 done
@@ -61,23 +63,23 @@ done
 
 # Install Desktop
 echo "-->Install desktop"
-# sudo yum -y groupinstall "Server with GUI"
-sudo yum -y groupinstall 'X Window System' 'GNOME'
+# yum -y groupinstall "Server with GUI"
+yum -y groupinstall 'X Window System' 'GNOME'
 
 echo "-->set default graphical target"
 # The below command will change runlevel from runlevel 3 to runelevel 5 
-sudo systemctl set-default graphical.target
+systemctl set-default graphical.target
 
 # skip the gnome initial setup
 echo "-->create file gnome-initial-setup-done to skip gnoe initial setup"
 for homeDir in $( find /home -mindepth 1 -maxdepth 1 -type d )
 do 
     confDir=$homeDir/.config
-    sudo mkdir -p $confDir
-    sudo chmod 777 $confDir
-    echo "yes" | sudo tee $confDir/gnome-initial-setup-done
-    sudo chmod 777 $confDir/gnome-initial-setup-done
+    mkdir -p $confDir
+    chmod 777 $confDir
+    echo "yes" | $confDir/gnome-initial-setup-done
+    chmod 777 $confDir/gnome-initial-setup-done
 done
 
 echo "-->start graphical target"
-sudo systemctl start graphical.target
+systemctl start graphical.target
